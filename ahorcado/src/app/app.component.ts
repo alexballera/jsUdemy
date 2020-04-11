ab863618d6c009e51bf1129e1c1b05e758786358
Alex Ballera <alexballera@gmail.com>
2020-04-11 14:29:19 -0300

    Mostrar letras correctas

--- ahorcado/src/app/app.component.ts
+++ ahorcado/src/app/app.component.ts
@@ -12,9 +12,19 @@ export class AppComponent {
   palabraOculta = '_ ';
 
   letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-            'K', 'L', 'M', 'N', 'Ã‘', 'O', 'P', 'Q', 'R', 'S',
+            'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
             'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   constructor() {
     this.palabraOculta = '_ '.repeat(this.palabra.length)
+  }
+
+  comprobar(letra) {
+    const palabraOcultaArr = this.palabraOculta.split(' ');
+    for (let i = 0; i < this.palabra.length; i++) {
+      if (this.palabra[i] === letra) {
+        palabraOcultaArr[i] = letra;
+      }
+    }
+    this.palabraOculta = palabraOcultaArr.join(' ');
   }
 }
